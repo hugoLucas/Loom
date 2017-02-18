6135aec982afa0a876658c21c238c6c29b64e4e7
hugoLucas <hugojlucas@gmail.com>
2017-02-17 19:23:05 -0800

    Saving work

--- src/Main.java
+++ src/Main.java
@@ -2,25 +2,33 @@ import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
 import org.omg.CORBA.portable.InputStream;
 
-import java.io.File;
+import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+        public static void main(String[] args) throws Exception {
+            String inputString = fileToString("C:\\Users\\Hugo\\IdeaProjects\\Loom\\LoomInput_Chapter");
+            ANTLRInputStream input = new ANTLRInputStream(inputString);
+            LoomLexer lexer = new LoomLexer(input);
 
-        File inputFile = new File("C:\\Users\\Hugo Lucas\\IdeaProjects\\Stock\\input");
-
-        ANTLRInputStream input = new ANTLRInputStream(System.in);
-
-        StockLexer lexer = new StockLexer(input);
-
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-
-        for (Token token = lexer.nextToken(); token.getType() != Token.EOF; token = lexer.nextToken()) {
-            System.out.println(token.toString());
+            for (Token token = lexer.nextToken(); token.getType() != Token.EOF; token = lexer.nextToken())
+                System.out.println(token.toString());
         }
-        // StockParser parser = new StockParser(tokens);
-        // ParseTree tree = parser.program(); // begin parsing at init rule
-        // System.out.println(tree.toStringTree(parser)); // print LISP-style tree
+
+        public static String fileToString(String path) throws IOException {
+            FileInputStream is = new FileInputStream(path);
+            BufferedReader buf = new BufferedReader(new InputStreamReader(is));
+
+            String line = buf.readLine();
+            StringBuilder sb = new StringBuilder();
+
+            while(line != null){
+                sb.append(line).append("\n");
+                line = buf.readLine();
+            }
+
+            return sb.toString();
+        }
     }
-}
